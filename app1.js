d63a650b3def134006345d7dda77df3958aa4b49
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-19 07:10:18 +0530

    app1.js

---
+++ app1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Football() {
+  const shoot = (a) => {
+    alert(a);
+  }
+
+  return (
+    <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+  );
+}
+
+ReactDOM.render(<Football />, document.getElementById('root'));
